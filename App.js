d18d944ef504df448f94ff5a18d9306c3fcc618b
sruthi2717 <134846946+sruthi2717@users.noreply.github.com>
2023-09-14 22:45:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import Homepage from './components/Homepage';
+import './App.css';
+import Navbar from './components/Navbar';
+import Ai1 from './components/Ai1';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Homepage/>
+      <Ai1/>
+    </div>
+  );
+}
+
+export default App;
